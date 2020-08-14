4ff6e9e194564b51a5715cda1d41541ff69ff8e3
Mudit Gupta <muditg317@gmail.com>
2020-08-13 20:01:54 -0400

    add timeout

--- backend/controllers/usersController.js
+++ backend/controllers/usersController.js
@@ -1,28 +1,29 @@
 const User = require('../models/Users');
+const { promiseTimeout } = require('../util/Utils');
 
 module.exports = {
   findAll: function(request, response) {
-    User.find(request.query)
+    promiseTimeout(5000, User.find(request.query))
       .then(books => response.json(books))
       .catch(err => response.status(422).json(err));
   },
   findById: function(request, response) {
-    User.findById(request.params.id)
+    promiseTimeout(5000, User.findById(request.params.id))
       .then(book => response.json(book))
       .catch(err => response.status(422).json(err));
   },
   create: function(request, response) {
-    User.create(request.body)
+    promiseTimeout(5000, User.create(request.body))
       .then(newUser => response.json(newUser))
       .catch(err => response.status(422).json(err));
   },
   update: function(request, response) {
-    User.findOneAndUpdate({ _id: request.params.id }, request.body)
+    promiseTimeout(5000, User.findOneAndUpdate({ _id: request.params.id }, request.body))
       .then(book => response.json(book))
       .catch(err => response.status(422).json(err));
   },
   remove: function(request, response) {
-    User.findById({ _id: request.params.id })
+    promiseTimeout(5000, User.findById({ _id: request.params.id }))
       .then(book => book.remove())
       .then(allbooks => response.json(allbooks))
       .catch(err => response.status(422).json(err));
---
+++ backend/util/Utils.js
@@ -0,0 +1,18 @@
+module.exports = {
+  promiseTimeout: (ms, promise) => {
+
+    // Create a promise that rejects in <ms> milliseconds
+    let timeout = new Promise((resolve, reject) => {
+      let id = setTimeout(() => {
+        clearTimeout(id);
+        reject({message: 'Timed out in '+ ms + 'ms.'});
+      }, ms)
+    })
+
+    // Returns a race between our timeout and the passed in promise
+    return Promise.race([
+      promise,
+      timeout
+    ]);
+  }
+};
